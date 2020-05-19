221bb130bfc6fc7c72b663051b365896586d06cb
m2maomao <m2maomao@163.com>
2020-05-19 22:40:11 +0800

    增加服务

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROS } from './mock-heros';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { 
 
-  getHeroes(): Observable<Hero []> {
-    return of(HEROS);
   }
 
+  getHeroes(): Observable<Hero []> {
+    return of(HEROS); // of(HEROS)会返回一个Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组。
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROS);
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,33 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
-
-  heroes: Hero [];
-  hero: Hero = {
-    id: 1,
-    name: 'Windowstorm'
-  };
   // 定义选择英雄
   selectedHero: Hero;
+
+  heroes: Hero [];
+  
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+  
 
 }
